7e2cfa25c2486497b5cb58ed598759454fa0ee22
Ryohei Kobayashi <kobaryo222912@gmail.com>
2023-04-04 12:45:59 +0900

    [fix] Union-Find

---
+++ src/DataStructure/union-find.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../template.hpp"
+
+/**
+ * @brief Union-Find
+ * @docs docs/union-find.md
+ */
+
+struct UnionFind {
+    vector<int> par;
+    UnionFind(int n) {
+        par.assign(n, -1);
+    };
+    int root(int x) {
+        if(par[x] < 0)
+            return x;
+        else
+            return par[x] = root(par[x]);
+    };
+    int size(int x) {
+        x = root(x);
+        return -1 * par[x];
+    };
+    bool unite(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if(x == y)
+            return false;
+        if(size(x) < size(y))
+            swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    bool same(int x, int y) {
+        return root(x) == root(y);
+    };
+};
---
+++ src/test/verify/aoj-dsl-1-a.test.cpp
@@ -0,0 +1,16 @@
+#include "../../template.hpp"
+#include "../../DataStructure/union-find.hpp"
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    UnionFind uf(n);
+    for(int i = 0; i < q; ++i) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if(com == 0)
+            uf.unite(x, y);
+        else
+            cout << uf.same(x, y) << endl;
+    }
+}
