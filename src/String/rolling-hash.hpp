8e8358cf1698d73c78f02ed970794037cb166bb2
Ryohei Kobayashi <kobaryo222912@gmail.com>
2023-11-02 03:59:01 +0900

    ロリハの基数を動的に生成するようにした #3

--- src/String/rolling-hash.hpp
+++ src/String/rolling-hash.hpp
@@ -11,20 +11,25 @@ struct Hash{
     rhmint1 hash1;
     rhmint2 hash2;
     int length;
-    Hash(rhmint1 h1, rhmint2 h2, int length){
-        
+    Hash(rhmint1 h1, rhmint2 h2, int length): hash1(h1), hash2(h2), length(length) {}
+    constexpr bool operator==(const Hash &b){
+        return hash1 == b.hash1 && hash2 == b.hash2;
     }
-}
+    constexpr bool operator!=(const Hash &b){
+        return hash1 != b.hash1 || hash2 != b.hash2;
+    }
+};
 
 struct RollingHash {
+    static rhmint1 base1;
+    static rhmint2 base2;
     vector<rhmint1> hash1, pow1;
     vector<rhmint2> hash2, pow2;
     string s;
     int n;
-    const rhmint1 base1 = 1009;
-    const rhmint2 base2 = 1007;
     RollingHash(string _s) {
         s = _s;
+        if(base1 == 0) generate_base();
         build();
     };
     void build(){
@@ -33,27 +38,38 @@ struct RollingHash {
         hash2.resize(n + 1);
         pow1.resize(n + 1);
         pow2.resize(n + 1);
-        hash1[0] = mint1(s[0]);
-        hash2[0] = mint2(s[0]);
+        hash1[0] = rhmint1(s[0]);
+        hash2[0] = rhmint2(s[0]);
         pow1[0] = 1;
         pow2[0] = 1;
         for(int i = 0; i < n; ++i) {
-            hash1[i + 1] = hash1[i] * base1 + mint1(s[i]);
-            hash2[i + 1] = hash2[i] * base2 + mint2(s[i]);
+            hash1[i + 1] = hash1[i] * base1 + rhmint1(s[i]);
+            hash2[i + 1] = hash2[i] * base2 + rhmint2(s[i]);
             pow1[i + 1] = pow1[i] * base1;
             pow2[i + 1] = pow2[i] * base2;
         }
     }
+    // 参考: https://ei1333.github.io/library/string/rolling-hash.hpp
+    static void generate_base() {
+        mt19937_64 mt(chrono::steady_clock::now().time_since_epoch().count());
+        uniform_int_distribution< uint64_t > rand1(1, rhmod1 - 1);
+        uniform_int_distribution< uint64_t > rand2(1, rhmod2 - 1);
+        base1 = rhmint1(rand1(mt));
+        base2 = rhmint2(rand2(mt));
+    }
     Hash get(int l, int r) {
         rhmint1 h1 = hash1[r] - hash1[l] * pow1[r - l];
         rhmint2 h2 = hash2[r] - hash2[l] * pow2[r - l];
-        return Hash(h1.get(), h2.get());
+        return Hash(h1, h2, r - l);
     }
-    Hash concat(Hash a, Hash b, int len_b){
-        rhmint1 h1a = a.first, h1b = b.first;
-        rhmint2 h2a = a.second, h2b = b.second;
-        rhmint1 h1 = h1a * pow1[len_b] + h1b;
-        rhmint2 h2 = h2a * pow2[len_b] + h2b;
-        return Hash(h1.get(), h2.get());
+    Hash concat(Hash a, Hash b){
+        rhmint1 h1a = a.hash1, h1b = b.hash1;
+        rhmint2 h2a = a.hash2, h2b = b.hash2;
+        rhmint1 h1 = h1a * pow1[b.length] + h1b;
+        rhmint2 h2 = h2a * pow2[b.length] + h2b;
+        return Hash(h1, h2, a.length + b.length);
     }
 };
+
+rhmint1 RollingHash::base1 = 0;
+rhmint2 RollingHash::base2 = 0;
