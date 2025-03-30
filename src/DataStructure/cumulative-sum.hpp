e145eeb37dc8357741ca032aa2c29c586f653128
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-03-30 10:43:43 +0900

    軽微な修正

--- src/DataStructure/cumulative-sum.hpp
+++ src/DataStructure/cumulative-sum.hpp
@@ -1,7 +1,13 @@
 #pragma once
+
+/**
+ * @brief 累積和
+ */
+
 #include <vector>
 #include <cassert>
 #include <concepts>
+#include "../Util/debug.hpp"
 
 template <std::integral T>
 struct CumulativeSum {
@@ -11,6 +17,7 @@ struct CumulativeSum {
   public:
     int n;
     std::vector<T> data;
+
     CumulativeSum(int n)
         : n(n), data(n + 1, T()) {}
     CumulativeSum(const std::vector<T> &a)
@@ -19,43 +26,22 @@ struct CumulativeSum {
             add(i, a[i]);
         build();
     }
-    /**
-     * @brief 累積和を構築する
-     *
-     */
+
     void build() {
-#ifdef LOCAL
-        assert(!built);
-#endif
+        debug_assert(!built);
         for(int i = 0; i < n; i++) {
             data[i + 1] = data[i] + data[i + 1];
         }
         built = true;
     }
-    /**
-     * @brief data[idx + 1] += x
-     *
-     * @param idx 加算する位置 (0-indexed)
-     * @param x 加算する値
-     */
+
     void add(int idx, T x) {
-#ifdef LOCAL
-        assert(!built);
-#endif
-        assert(!built);
+        debug_assert(!built);
         data[idx + 1] += x;
     }
-    /**
-     * @brief [l, r) の和, data[r] - data[l]
-     *
-     * @param l 左端の位置 (0-indexed, 閉区間)
-     * @param r 右端の位置 (0-indexed, 開区間)
-     * @return T 区間の総和
-     */
+
     T sum(int l, int r) {
-#ifdef LOCAL
-        assert(built);
-#endif
+        debug_assert(built);
         return data[r] - data[l];
     }
 };
