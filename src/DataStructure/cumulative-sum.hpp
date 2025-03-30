9376b26ef8a6a31c05f281f58177752377f82f85
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-03-30 09:55:50 +0900

    累積和の assert をいい感じに

--- src/DataStructure/cumulative-sum.hpp
+++ src/DataStructure/cumulative-sum.hpp
@@ -1,15 +1,20 @@
-template <typename T = long long int>
+#pragma once
+#include <vector>
+#include <cassert>
+#include <concepts>
+
+template <std::integral T>
 struct CumulativeSum {
   private:
     bool built = false;
 
   public:
     int n;
-    vector<T> wa;
+    std::vector<T> data;
     CumulativeSum(int n)
-        : n(n), wa(n + 1, T(0)) {}
-    CumulativeSum(const vector<T> &a)
-        : n((int)a.size()), wa(n + 1, T(0)) {
+        : n(n), data(n + 1, T()) {}
+    CumulativeSum(const std::vector<T> &a)
+        : n((int)a.size()), data(n + 1, T()) {
         for(int i = 0; i < n; i++)
             add(i, a[i]);
         build();
@@ -19,31 +24,38 @@ struct CumulativeSum {
      *
      */
     void build() {
+#ifdef LOCAL
         assert(!built);
+#endif
         for(int i = 0; i < n; i++) {
-            wa[i + 1] = wa[i] + wa[i + 1];
+            data[i + 1] = data[i] + data[i + 1];
         }
         built = true;
     }
     /**
-     * @brief wa[idx + 1] += x
+     * @brief data[idx + 1] += x
      *
      * @param idx 加算する位置 (0-indexed)
      * @param x 加算する値
      */
     void add(int idx, T x) {
+#ifdef LOCAL
         assert(!built);
-        wa[idx + 1] += x;
+#endif
+        assert(!built);
+        data[idx + 1] += x;
     }
     /**
-     * @brief [l, r) の和, wa[r] - wa[l]
+     * @brief [l, r) の和, data[r] - data[l]
      *
      * @param l 左端の位置 (0-indexed, 閉区間)
      * @param r 右端の位置 (0-indexed, 開区間)
      * @return T 区間の総和
      */
     T sum(int l, int r) {
+#ifdef LOCAL
         assert(built);
-        return wa[r] - wa[l];
+#endif
+        return data[r] - data[l];
     }
 };
